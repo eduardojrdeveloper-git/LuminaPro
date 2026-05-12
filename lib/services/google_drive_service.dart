0bcd666eb87b6e13ea2e342f1997dd111268af5d
Eduardo <eduardo.jr.gdrive@gmail.com>
2026-05-11 23:20:36 -0500

    fix: resolve missing imports and metadata getter errors

--- lib/services/google_drive_service.dart
+++ lib/services/google_drive_service.dart
@@ -6,6 +6,7 @@ import 'package:http/http.dart' as http;
 import 'package:path_provider/path_provider.dart';
 import 'package:path/path.dart' as p;
 import 'package:metadata_god/metadata_god.dart';
+import '../main.dart' show extractCloudCoversNotifier;
 import 'log_service.dart';
 import 'library_service.dart';
 
