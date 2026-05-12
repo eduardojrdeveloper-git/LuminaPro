fbe4daca37a52eb101a394d7ede2751809be15c3
Eduardo <eduardo.jr.gdrive@gmail.com>
2026-05-12 00:24:26 -0500

    fix: import dart:convert for jsonEncode/jsonDecode

--- lib/services/library_service.dart
+++ lib/services/library_service.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 import 'dart:typed_data';
+import 'dart:convert';
 import 'package:flutter/foundation.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:path/path.dart' as p;
